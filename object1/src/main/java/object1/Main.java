fc26aaca6112916bc7a05b8d472b1f132513e559
Michael Scott <mjscott2002@gmail.com>
2024-02-29 17:19:15 -0500

    Added Chair array

--- object1/src/main/java/object1/Main.java
+++ object1/src/main/java/object1/Main.java
@@ -24,17 +24,31 @@ public class Main {
 
         System.out.println(car.brand + ", " + car.color + ", " + car.numwheels);
 
-        Car[] cars = new Car[10];
+        Car[] cars = new Car[3];
+
+        Chair[] chairs = new Chair[3];
 
         Car car1 = new Car("BMW", "Blue", 4);
         Car car2 = new Car("FordF150", "White", 4);
         Car car3 = new Car("Volvo", "Black", 4);
 
+        Chair chair1 = new Chair("Oak Wood", "Brown", 4);
+        Chair chair2 = new Chair("Plastic", "Black", 4);
+        Chair chair3 = new Chair("Burch Wood", "Light Brown", 4);
+
         cars[0] = car1;
         cars[1] = car2;
         cars[2] = car3;
 
+        chairs[0] = chair1;
+        chairs[1] = chair2;
+        chairs[2] = chair3;
+
+        System.out.println("\n\nChairs available in Store: " + chair1.material + "-" + chair1.color + ", " + chair2.material + "-" + chair2.color + ", " + chair3.material + "-" + chair3.color);
+
         System.out.println("\n\nCars available on lot A: " + car1.brand + "-" + car1.color + ", " + car2.brand + "-" + car2.color + ", " + car3.brand + "-" + car3.color);
+
+
     }
 
 
